b857342b1f2663b02ac2956042a7064b169378c6
Victor Panteleev <panteleevvitja32@gmail.com>
2020-08-25 17:04:27 +0300

    home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
-  return <div>kalkal</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+import { map } from "underscore";
+
+import "./Home.scss";
+
+import Header from "../Header/Header";
+
+import { ReactComponent as User } from "../../images/user.svg";
+import { ReactComponent as Star } from "../../images/star.svg";
+import { ReactComponent as Nurse } from "../../images/nurse.svg";
+import { ReactComponent as House } from "../../images/house.svg";
+import { ReactComponent as Clients } from "../../images/clientssvg.svg";
+import { ReactComponent as Messages } from "../../images/messages.svg";
+import { ReactComponent as Broadcast } from "../../images/brodsvg.svg";
+import { ReactComponent as Employees } from "../../images/emplo.svg";
+import { ReactComponent as Appointment } from "../../images/appointment.svg";
+
+const TITLE = "Домашняя";
+
+const SECTIONS = [
+  { title: "Приёмы", href: "/appointments", Icon: Appointment },
+  { title: "События", href: "/events", Icon: Star },
+  { title: "Оповещения", href: "/notifications", Icon: Broadcast },
+  { title: "Сообщения", href: "/messages", Icon: Messages },
+  { title: "Клиенты", href: "/clients", Icon: Clients },
+  { title: "Сотрудники", href: "/employees", Icon: Employees },
+];
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <Header
+          title={TITLE}
+          userName="Иванов Иван Иванович"
+          className="Home-Header"
+          renderIcon={() => <House className="Header-Icon" />}
+        />
+        <div className="Home-Body">
+          <div className="SectionNavigation">
+            {map(SECTIONS, ({ title, href, Icon }) => (
+              <a className="SectionNavigation-Item Section" href="#">
+                <Icon className="Section-Icon" />
+                <span className="Section-Title">{title}</span>
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
